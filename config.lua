e817036c541b1358c5bfe42e83e167d54ab02260
Owain <owain.jones74@gmail.com>
2020-10-29 22:52:26 +0000

    basis

---
+++ config.lua
@@ -0,0 +1,10 @@
+-- A lazy way at doing a config lol
+
+local config -- ignore this line
+
+config.key = "" -- Your Discord token. Self-botting is against ToS, do at your own risk lol
+
+config.limitToGuild = "" -- You can limit it to a specific guild here. Leave blank or as false to disable. This also supports a table.
+-- Example of a table: config.limitToGuild = {"123", "456"}
+
+return config
